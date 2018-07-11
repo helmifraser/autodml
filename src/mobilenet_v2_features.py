a00612e7416b7c57f6e35cb3148459353db0f69e
Helmi Fraser <helmifraser@github.com>
2018-07-11 17:45:16 +0100

    Started implementing online image data augmentation

--- src/mobilenet_v2_features.py
+++ src/mobilenet_v2_features.py
@@ -5,13 +5,16 @@ import keras
 
 from keras.applications.mobilenetv2 import MobileNetV2, preprocess_input, decode_predictions
 from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 from keras.models import Model, load_model
 from scipy.misc import imsave
 
 np.random.seed(420)  # for reproducibility
 DATA_HEADERS = ["frame_no", "steer", "throttle", "brake", "reverse"]
+COLS_TO_USE = [1, 2, 3]
 
-def obtain_episode_data(folder_path, seg=False, delim=' ', headers=None):
+def obtain_episode_data(folder_path, seg=False, delim=' ', headers=None,
+                        use_index=False, col_index=0, columns_to_use=COLS_TO_USE):
     # Obtain images i.e x data
     rgb_path = folder_path+"/CameraRGB"
     rgb_images = []
@@ -34,11 +37,17 @@ def obtain_episode_data(folder_path, seg=False, delim=' ', headers=None):
 
     for filename in os.listdir(folder_path):
         if filename.endswith("_episode.txt"):
-            dataframe = pd.read_csv(folder_path+"/"+filename, delimiter=delim, names=headers, index_col=0)
+            if use_index == True:
+                dataframe = pd.read_csv(folder_path+"/"+filename,
+                                        delimiter=delim, names=headers,
+                                        index_col=col_index, usecols=columns_to_use)
+            else:
+                dataframe = pd.read_csv(folder_path+"/"+filename,
+                                        delimiter=delim,
+                                        names=headers, usecols=columns_to_use)
 
     dataset = dataframe.values
     control_data = dataset[:].astype(float)
-
     return rgb_images, control_data
 
 def image_extract(folder_path, number_of_images=600):
@@ -48,34 +57,49 @@ def image_extract(folder_path, number_of_images=600):
             if filename.endswith(".png"):
                 pos = filename.find("_")
                 id = int(filename[:pos])
-                img = image.load_img(os.path.join(folder_path, filename), target_size=(224, 224))
-                images[id] = image.img_to_array(img)
+                img = load_img(os.path.join(folder_path, filename), target_size=(224, 224))
+                images[id] = img_to_array(img)
                 images[id] = np.expand_dims(images[id], axis=0)
                 images[id] = preprocess_input(images[id])
-                # print(shape(rgb_images[id]))
-                # print("Shape: {}".format(np.shape(rgb_images[1])))
+                # print(shape(images[id]))
+                # print("Shape: {}".format(np.shape(images[1])))
 
         return images
 
+datagen = ImageDataGenerator(
+        rotation_range=45,
+        rescale=1./255,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        fill_mode='nearest')
+
 x_train, y_train = obtain_episode_data("carla_dataset/train/0_town_1", headers=DATA_HEADERS)
+
+# x_batch = datagen.flow(x_train, batch_size=1)
+
+# the .flow() command below generates batches of randomly transformed images
+# and saves the results to the `preview/` directory
+# i = 0
+# for batch in datagen.flow(x_train[0], batch_size=1,
+#                           save_to_dir='preview', save_prefix='test', save_format='png'):
+#     i += 1
+#     if i > 20:
+#         break  # otherwise the generator would loop indefinitely
 
 print("Shape x_train: {}".format(np.shape(x_train)))
 print("Shape y_train: {}".format(np.shape(y_train)))
+# print("Shape x_batch: {}".format(np.shape(x_batch)))
 
-
-# Removes the top and displays only features
-
-# model = MobileNetV2(input_shape=(224, 224, 3), alpha=1.0, depth_multiplier=1, include_top=False, weights='imagenet', pooling='avg')
-
-# img_path = 'elephant.jpg'
-# img = image.load_img(img_path, target_size=(224, 224))
-# x = image.img_to_array(img)
-# x = np.expand_dims(x, axis=0)
-# x = preprocess_input(x)
-# print("Shape x: {}".format(np.shape(x)))
-#
-# features = model.predict(x)
-#
-# # model.summary()
-#
-# print("Shape: {}".format(np.shape(features)))
+# here's a more "manual" example
+# for e in range(1, 11):
+#     print('Epoch', e)
+    # batches = 0
+    # for x_batch in datagen.flow(x_train, batch_size=32):
+    #     model.fit(x_batch, y_train)
+    #     batches += 1
+    #     if batches >= len(x_train) / 32:
+    #         # we need to break the loop by hand because
+    #         # the generator loops indefinitely
+    #         break
